f28431d346b482de3b077150bdb0d547fd77723e
Maria <gilmartinm222@gmail.com>
2024-05-08 18:36:54 +0200

    Commit 1

---
+++ include/script.php
@@ -0,0 +1,5 @@
+<!-- Versión bundle de Bootstrap JS 
+incluir antes de los scripts de cada pagina -->
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js">
+</script>
