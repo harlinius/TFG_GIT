f7a773be9c0f4f1fb92b6007cf9b98f6026a5366
Maria <gilmartinm222@gmail.com>
2024-05-08 22:05:45 +0200

    commit

--- gestion_cuentas/login.php
+++ gestion_cuentas/login.php
@@ -1,3 +1,11 @@
 <?php
+$tituloPagina = "Read&Meet | Login";
+require_once __DIR__ . "/include/cabecera_login.php";
+?>
 
+<?php
+    require_once __DIR__ . "/include/script.php";
+    ?>
+<?php
+require_once __DIR__ . "/include/pie.php";
 ?>
