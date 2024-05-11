c90cd175637d42934db10bcc23ff05cdb7aa1901
Maria <gilmartinm222@gmail.com>
2024-05-11 15:56:29 +0200

    Commit

--- gestion_cuentas/login.php
+++ gestion_cuentas/login.php
@@ -8,48 +8,45 @@ $tituloPagina = "Read&Meet | Login";
 require_once "../include/cabecera_login.php";
 ?>
 
-<div class="row">
-    <div class="col-md-4 offset-md-4">
-
+<div id="bloque_form" class="row">
+    <div class="col-md-4 offset-md-2">
+        <h1>Read&Meet</h1>
+    </div>
+    <div class="col-md-3">
         <form action="doLogin.php" method="POST" class="row">
-            <h1>Iniciar sesión</h1>
-            <?php 
-                if (isset($_SESSION['error-login'])):  ?>
-                    <div class="alert alert-danger">
-                        <?= e($_SESSION['error-login']) ?>
-                    </div>
-            <?php 
-                    unset($_SESSION['error-login']);
-                endif; ?>
+            <?php
+            if (isset($_SESSION['error-login'])) :  ?>
+                <div class="alert alert-danger">
+                    <?= e($_SESSION['error-login']) ?>
+                </div>
+            <?php
+                unset($_SESSION['error-login']);
+            endif; ?>
             <div class="mb-3">
                 <label class="form-label" for="login">
                     Nombre de usuario
                 </label>
-                <input type="text" id="login" name="login"
-                    class="form-control" 
-                    placeholder="Nombre" />
+                <input type="text" id="login" name="login" class="form-control" placeholder="Nombre" />
             </div>
             <div class="mb-3">
                 <label class="form-label" for="pwd">
                     Contraseña
                 </label>
-                <input type="password" id="pwd" name="pwd"
-                    class="form-control" 
-                    placeholder="Contraseña" />
+                <input type="password" id="pwd" name="pwd" class="form-control" placeholder="Contraseña" />
             </div>
             <div class="mb-3 text-center">
+                <a href="crearCuenta.php" class="btn btn-secondary">Crear cuenta</a>
                 <button type="submit" class="btn btn-primary">
                     Iniciar sesión
                 </button>
-                <a href="crearCuenta.php" class="btn btn-secondary">Crear cuenta</a>
-            </div>  
+            </div>
         </form>
     </div>
 </div>
 
 <?php
-    require_once "../include/script.php";
-    ?>
+require_once "../include/script.php";
+?>
 <?php
 require_once "../include/pie_login.php";
 ?>
--- include/cabecera_login.php
+++ include/cabecera_login.php
@@ -9,7 +9,7 @@
     <!-- CSS Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href="css/estilo_login.css">
+    <link rel="stylesheet" href="../css/estilo_login.css">
     <title><?= $tituloPagina ?></title>
     <link rel="icon" href="../css/icono_tab.png">
 </head>
