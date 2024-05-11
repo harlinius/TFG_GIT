f2d249f0f37b0791b64b0834f26ed5f2a070af7f
Maria <gilmartinm222@gmail.com>
2024-05-11 17:08:17 +0200

    Slider 1

--- gestion_cuentas/login.php
+++ gestion_cuentas/login.php
@@ -9,10 +9,12 @@ require_once "../include/cabecera_login.php";
 ?>
 
 <div id="bloque_form" class="row">
-    <div class="col-md-4 offset-md-2">
+    <div class="col-md-4 col-sm-12 offset-md-2">
+        <p>Bienvenido a</p>
         <h1>Read&Meet</h1>
+        <p id="autor">By María Gil Martín</p>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 col-sm-12 ">
         <form action="doLogin.php" method="POST" class="row">
             <?php
             if (isset($_SESSION['error-login'])) :  ?>
@@ -44,6 +46,48 @@ require_once "../include/cabecera_login.php";
     </div>
 </div>
 
+<div id="carrusel" class="row">
+    <div class="col-md-7 col-sm-12 offset-md-2">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="3000">
+                    <img src="../css/imagenes/slider1.jpg" class="imagen_carrusel d-block w-100 carousel-fade " alt="Mano agarrando libros de una librería">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Lee</h5>
+                        <p>tus propios libros online.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="3000">
+                    <img src="../css/imagenes/slider2.jpg" class="imagen_carrusel d-block w-100 carousel-fade "  alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Descubre</h5>
+                        <p>nuevos libros y géneros.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" data-bs-interval="3000">
+                    <img src="../css/imagenes/slider3.jpg" class="imagen_carrusel d-block w-100 carousel-fade " alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Conecta</h5>
+                        <p>con personas con tus mismos gustos.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</div>
 <?php
 require_once "../include/script.php";
 ?>
