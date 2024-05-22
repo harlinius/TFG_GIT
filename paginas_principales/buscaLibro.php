eb4dc21b3083239d76ba0dd1637e2367f150b9c7
Maria <gilmartinm222@gmail.com>
2024-05-22 22:29:59 +0200

    acceso denegado busca libro

--- paginas_principales/buscaLibro.php
+++ paginas_principales/buscaLibro.php
@@ -1,6 +1,8 @@
 <?php
 require_once '../bd/libro.php';
 
+
+
 if (isset($_POST['buscador'])) {
     $titulo = $_POST['buscador'];
     $libros_coinciden = Libro::busca_por_titulo($titulo);
@@ -8,7 +10,6 @@ if (isset($_POST['buscador'])) {
     header('Content-Type: application/json');
     echo json_encode($libros_coinciden);
 } else {
-    header('Content-Type: application/json');
-    echo json_encode([]);
+    echo "Acceso denegado. <a href='../gestion_cuentas/login.php' class='btn btn-secondary'>Volver</a>";
 }
 ?>
