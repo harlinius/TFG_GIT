219a0e61deb0a1cb504e96ae631135e9dbcde012
Maria <gilmartinm222@gmail.com>
2024-05-23 19:34:47 +0200

    botones

--- paginas_principales/libro.php
+++ paginas_principales/libro.php
@@ -32,24 +32,36 @@ if ($usuario->administrador == 1) {
         <h1><?php echo "$libro->titulo" ?></h1>
     </div>
     <div class="botones col-lg-12">
-        <?php if (!isset($esta_biblioteca)) { 
+        <?php if (!isset($esta_biblioteca)) {
             echo ('
-            <a href="../gestion_biblioteca/anadir_a_biblioteca.php?id_libro=' . $libro->id_libro . '" class="btn no_pendiente">
+            <a href="../gestion_biblioteca/anadir_a_biblioteca_pendiente.php?id_libro=' . $libro->id_libro . '" class="btn no_pendiente">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-star-fill" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5M8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.18.18 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.18.18 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.18.18 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.18.18 0 0 1-.134-.098z" />
                 </svg>
                 Guardar como pendiente
+            </a>
+            <a href="../gestion_biblioteca/anadir_a_biblioteca_leyendo.php?id_libro=' . $libro->id_libro . '" class="btn anadir_leyendo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+                  <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                </svg>
+                Guardar como leyendo
+            </a>
+            <a href="../gestion_biblioteca/anadir_a_biblioteca_acabado.php?id_libro=' . $libro->id_libro . '" class="btn anadir_acabado">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book-fill" viewBox="0 0 16 16">
+                    <path d="M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                </svg>
+                Guardar como acabado
             </a>');
-        } 
-        else {
+        } else {
             echo ('
-            <a href="../gestion_biblioteca/quitar_de_biblioteca.php?id_libro=' . $libro->id_libro . '" class="btn pendiente">
+            <a href="../gestion_biblioteca/quitar_de_biblioteca_pendiente.php?id_libro=' . $libro->id_libro . '" class="btn pendiente">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-star-fill" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5M8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.18.18 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.18.18 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.18.18 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.18.18 0 0 1-.134-.098z" />
                 </svg>
                 Eliminar de la biblioteca
             </a>
-            ');}
+            ');
+        }
         ?>
     </div>
     <div class="portada_libro col-lg-12 ">
